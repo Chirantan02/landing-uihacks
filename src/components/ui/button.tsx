e4d609f6338258f8e44c83a9e19e56355196ff59
Chirantan02 <chirantanbhardwaj@gmail.com>
2025-04-16 23:45:40 +0530

    commit b4 hosting

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,5 +1,6 @@
+'use client';
+
 import * as React from 'react';
-import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
@@ -39,24 +40,16 @@ export interface ButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, isLoading = false, children, ...props }, ref) => {
-    const Comp = asChild ? Slot : 'button';
-
     return (
-      <Comp
+      <button
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         disabled={isLoading || props.disabled}
         {...props}
       >
-        {isLoading ? (
-          <>
-            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-            {children}
-          </>
-        ) : (
-          children
-        )}
-      </Comp>
+        {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+        {children}
+      </button>
     );
   }
 );
